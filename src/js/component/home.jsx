46b5955c96ce8a713a64d169fd0acfc59054ee7b
Mercedes0708 <144907375+Mercedes0708@users.noreply.github.com>
2024-01-16 12:08:19 +0000

    traffic light terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
+export function Home() {
+    const [selectedColor, setSelectedColor] = useState("yellow");
+    const [showPurple, setShowPurple] = useState(false);
+    const [timerActive, setTimerActive] = useState(false);
+    useEffect(() => {
+        let intervalId;
+        if (timerActive) {
+            intervalId = setInterval(() => {
+                if (selectedColor === "red") {
+                    setSelectedColor("yellow");
+                } else if (selectedColor === "yellow") {
+                    setSelectedColor("green");
+                } else if (selectedColor === "green") {
+                    setSelectedColor("red");
+                }
+            }, 2000);
+        }
+        return () => clearInterval(intervalId);
+    }, [timerActive, selectedColor]);
+    const toggleTimer = () => {
+        setTimerActive(!timerActive);
+    };
+    return (
+        <div>
+            <div className="traffic-light text-center">
+                <div
+                    onClick={() => {
+                        setSelectedColor("red");
+                        setShowPurple(false); // Oculta el morado
+                    }}
+                    className={"light red" + (selectedColor === "red" ? " glow1" : "")}
+                ></div>
+                <div
+                    onClick={() => {
+                        setSelectedColor("yellow");
+                        setShowPurple(false); // Oculta el morado
+                    }}
+                    className={"light yellow" + (selectedColor === "yellow" ? " glow2" : "")}
+                ></div>
+                <div
+                    onClick={() => {
+                        setSelectedColor("green");
+                        setShowPurple(false); // Oculta el morado
+                    }}
+                    className={"light green" + (selectedColor === "green" ? " glow3" : "")}
+                ></div>
+                {showPurple && (
+                    <div
+                        onClick={() => {
+                            setSelectedColor("purple");
+                            setShowPurple(true);
+                        }}
+                        className={"light purple" + (selectedColor === "purple" ? " glow4" : "")}
+                    ></div>
+                )}
+            </div>
+            <button type="button" class="btn btn-secondary boton" onClick={() => setShowPurple(!showPurple)}>Uno más!</button>
+            <button type="button" className="btn btn-secondary boton2" onClick={toggleTimer}>
+                {timerActive ? "Detener Temporizador" : "Iniciar Temporizador"}
+            </button>
+        </div>
+    );
+}
 export default Home;
